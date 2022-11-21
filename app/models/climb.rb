539c1483ce3aa1dbb3e039cf53ecbdb088205d9e
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-21 17:46:52 -0500

    finished validations and associations for models

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,4 +1,10 @@
 class Climb < ApplicationRecord
+  STATUSES = %w[completed attempted retreated]
+
   belongs_to :line
   belongs_to :user
+
+  validates :status, presence: true, inclusion: { in: STATUSES }
+  validates :description, presence: true
+  validates_comparison_of :completion_date, less_than_or_equal_to: Date.today
 end
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,12 +1,12 @@
 class Line < ApplicationRecord
-  TYPES = %w[trad sport toprope]
+  CATEGORIES = %w[trad sport toprope]
 
   belongs_to :area
   has_many :climbs
   has_many :tips, dependent: :destroy
 
   validates :name, presence: true
-  validates :grade, presence: true
-  validates :type, presence: true
+  validates :grade, presence: true, inclusion: { in: GRADES_LIST }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :description, presence: true
 end
