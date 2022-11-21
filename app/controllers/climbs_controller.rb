d00594d9697d8950a6f19e073c2c5e1685dc9fd2
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-21 15:36:18 -0500

    Added Controllers

---
+++ app/controllers/climbs_controller.rb
@@ -0,0 +1,2 @@
+class ClimbsController < ApplicationController
+end
---
+++ test/controllers/climbs_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ClimbsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
