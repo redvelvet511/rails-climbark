bfdb777e42590a3034681c1e8c147728ff16d847
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-21 17:10:12 -0500

    added some validation and associations to models

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,3 +1,12 @@
 class Line < ApplicationRecord
+  TYPES = %w[trad sport toprope]
+
   belongs_to :area
+  has_many :climbs
+  has_many :tips, dependent: :destroy
+
+  validates :name, presence: true
+  validates :grade, presence: true
+  validates :type, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :climbs, dependent: :destroy
+  has_many :tips, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
 end
