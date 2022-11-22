7015330aa5899aeba77c4d45a4747d3a69d3604e
Nifemi Oyekunle <nifemioyekunle@gmail.com>
2022-11-22 12:00:28 -0500

    as a user/visitor i can browse all routes

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,6 @@
 class AreasController < ApplicationController
+  def index
+    @areas = Area.all
+    @areas = policy_scope(Area)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :areas, only: %i[index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
