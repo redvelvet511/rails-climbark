c5717e136146dfeda20d78ca64e53772da292e6e
Nifemi Oyekunle <nifemioyekunle@gmail.com>
2022-11-22 14:01:28 -0500

    user can see index of routs in area

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,2 +1,8 @@
 class LinesController < ApplicationController
+  def index
+    @area = Area.find(params[:area_id])
+    # @lines.area = @area
+    @lines = Line.all
+    @lines = policy_scope(Line)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,38 @@ require 'faker'
 
 Area.destroy_all
 User.destroy_all
-
+Line.destroy_all
 
 user = User.new(email: "climbark@gmail.com", username: "climbark", password: 123456)
 user.save!
 p "created #{User.count} users."
 
 5.times do
-  area = Area.new(name: Faker::Nation.capital_city )
+  area = Area.create!(
+    name: Faker::Nation.capital_city
+  )
   area.save!
 end
+rand(3..5).times do
+  line = Line.create!(
+    name: Faker::Address.street_name,
+    grade: Line::GRADES_LIST.sample, category: Line::CATEGORIES.sample,
+    description: Faker::Movie.quote,
+    area: Area.first
+  )
+  line.save!
+end
+
+
+# 5.times do
+#   area = Area.new(
+#     name: Faker::Nation.capital_city
+#   )
+#   area.save!
+#   rand(3..5).times do
+#     line = Line.new(
+#       name: Faker::Address.street_name
+#     )
+#     line.save!
+#   end
+# end
