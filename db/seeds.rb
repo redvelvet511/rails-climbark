ca1003c64c57403977db6dcda0872c7187fb5e7c
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-22 15:51:03 -0500

    scraped information from mountain project and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "json"
+
+Line.destroy_all
+Area.destroy_all
+
+area_lines_data = JSON.parse(File.read("db/areas_lines.json"))
+
+area_lines_data.each do |area|
+  if area["lines"].empty?
+    p "Skipped #{area["name"]} as it has no lines."
+    next
+  end
+
+  new_area = Area.new(name: area["name"])
+  new_area.save!
+  p "Created area #{new_area.name}."
+
+  area["lines"].each do |line|
+    new_line = Line.new(
+      name: line["name"],
+      grade: line["grade"],
+      category: line["category"],
+      description: line["description"],
+      location: line["location"],
+      protection: line["protection"],
+      area: new_area
+    )
+    new_line.save
+  end
+  p "Created #{new_area.lines.count} line(s) for #{new_area.name}."
+
+  if new_area.lines.empty?
+    new_area.destroy
+    p "Destroyed #{new_area.name} as it has no valid lines."
+  end
+end
+
+p "Created #{Area.count} areas in total!"
+p "Created #{Line.count} lines in total!"
