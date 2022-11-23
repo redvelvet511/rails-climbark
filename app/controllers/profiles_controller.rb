456afd02cc4f5757290f1ac9528793accb8fc7c3
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-23 11:25:32 -0500

    completed new tip action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,5 @@
 class ProfilesController < ApplicationController
   def show
-    @user = current_user
-    authorize(@user, policy_class: ProfilePolicy)
+    authorize(User, policy_class: ProfilePolicy)
   end
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,31 @@
 class TipsController < ApplicationController
+  before_action :set_line, only: %i[new create]
+
+  def new
+    @tip = Tip.new
+    authorize(@line, policy_class: TipPolicy)
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    @tip.line = @line
+    @tip.user = current_user
+    authorize(@line, policy_class: TipPolicy)
+
+    if @tip.save
+      redirect_to line_path(@line)
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def set_line
+    @line = Line.find(params[:line_id])
+  end
+
+  def tip_params
+    params.require(:tip).permit(:title, :content)
+  end
 end
