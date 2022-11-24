334bc2b2ccd8b158e006f654ab089a6b328b343b
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-24 14:23:35 -0500

    Now you can search for areas, routes and category

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -8,12 +8,11 @@ class AreasController < ApplicationController
   def search
     skip_authorization
     sql_query = "name ILIKE :query OR category ILIKE :query"
-    if params[:query].present?
-      if Area.where("name ILIKE ?", "%#{params[:query]}%").exists?
-        @search = Area.where("name ILIKE ?", "%#{params[:query]}%")
-      elsif Line.where(sql_query, query: "%#{params[:query]}%").exists?
-        @search = Line.where(sql_query, query: "%#{params[:query]}%")
-      end
+    case params[:query].present?
+    when Area.where("name ILIKE ?", "%#{params[:query]}%").exists?
+      @search = Area.where("name ILIKE ?", "%#{params[:query]}%")
+    when Line.where(sql_query, query: "%#{params[:query]}%").exists?
+      @search = Line.where(sql_query, query: "%#{params[:query]}%")
     else
       @search = Area.all
     end
