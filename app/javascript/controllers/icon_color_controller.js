4909f93b9b7e77274c3d330c1ef568d3f704a3ec
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-29 17:42:40 -0500

    Trying outsome stuff

--- app/javascript/controllers/icon_color_controller.js
+++ app/javascript/controllers/icon_color_controller.js
@@ -4,4 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
+
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', function onClick() {
+    btn.style.color = 'white';
+  });
 }
