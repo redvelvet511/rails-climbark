ace83063a3c90d02c49e5f76b77c32f840999c6b
Nifemi Oyekunle <nifemioyekunle@gmail.com>
2022-11-30 10:55:36 -0500

    profile pics are saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
   # geocoded_by :ip_address, latitude: :lat, longitude: :lon
   # after_validation :geocode
 
+  has_one_attached :photo   # default_url: "user_placeholder.jpg"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :climbs, dependent: :destroy
   has_many :lines, through: :climbs
   has_many :tips, dependent: :destroy
-  has_one_attached :photo   # default_url: "user_placeholder.jpg"
 
   validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
 end
