c8e22abefd95ec372541dbc46d54f24e0ff2c71b
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-30 11:15:36 -0500

    added more style to all climbs page

--- app/javascript/controllers/line_photo_controller.js
+++ app/javascript/controllers/line_photo_controller.js
@@ -2,24 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="line-photo"
 export default class extends Controller {
-  static targets = ["details", "toggler", "noPhotoMessage"]
+  static targets = ["details", "toggler"]
 
   connect() {
+    this.noPhotoMessage = document.getElementById("no-photo-message")
   }
 
   displayPhoto() {
     this.detailsTarget.classList.add("d-none")
     this.togglerTarget.classList.remove("d-none")
-    if (this.noPhotoMessageTarget) {
-      this.noPhotoMessageTarget.classList.remove("d-none")
+    if (this.noPhotoMessage) {
+      this.noPhotoMessage.classList.remove("d-none")
     }
   }
 
   hidePhoto() {
     this.togglerTarget.classList.add("d-none")
     this.detailsTarget.classList.remove("d-none")
-    if (this.noPhotoMessageTarget) {
-      this.noPhotoMessageTarget.classList.add("d-none")
+    if (this.noPhotoMessage) {
+      this.noPhotoMessage.classList.add("d-none")
     }
   }
 }
