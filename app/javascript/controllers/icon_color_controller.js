02415e3cfd4f3c4fd74a95e63f1379037fc285f4
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-29 23:26:18 -0500

    Change icon color where it is on page

--- app/javascript/controllers/icon_color_controller.js
+++ app/javascript/controllers/icon_color_controller.js
@@ -3,11 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="icon-color"
 export default class extends Controller {
   connect() {
+    switch (window.location.pathname) {
+      case "/home":
+        document.getElementById("footer-home").style.color = 'white'
+        break
+      case "/map":
+        document.getElementById("footer-map").style.color = 'white'
+        break
+      case "/search":
+        document.getElementById("footer-search").style.color = 'white'
+        break
+      case "/profile":
+        document.getElementById("footer-profile").style.color = 'white'
+        break
+    }
   }
-
-  const btn = document.getElementById('btn');
-
-  btn.addEventListener('click', function onClick() {
-    btn.style.color = 'white';
-  });
 }
