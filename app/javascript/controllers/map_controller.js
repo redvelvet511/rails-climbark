ca6937e2d8d724adeb1357a7d184f84384b4e8b9
Lisandra Bautista <lsndra.br@gmail.com>
2022-12-01 10:24:12 -0500

    Removed unecessary code

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,9 +18,6 @@ export default class extends Controller {
     this.#addMyMarkerToMap()
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
