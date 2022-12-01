57c7c039c561e04cc20c6df832bbe16a352585ca
Jin Qi <jenny_qiqu@hotmail.com>
2022-12-01 15:30:51 -0500

    updated seed for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,55 +3,75 @@ Climb.destroy_all
 User.destroy_all
 p "Removed users, climbs and tips from database."
 
-User.create!(email: "climbark@gmail.com", username: "Climbark", password: 123456)
-User.create!(email: "sandra@climbark.com", username: "Redvelvet", password: 123456)
+# User.create!(email: "climbark@gmail.com", username: "Climbark", password: 123456)
+User.create!(email: "sandra@climbark.com", username: "Sandra24", password: 123456)
 User.create!(email: "jenny@climbark.com", username: "Komezu", password: 123456)
-User.create!(email: "nifemi@climbark.com", username: "Nifemi", password: 123456)
+User.create!(email: "nifemi@climbark.com", username: "Nifemi21", password: 123456)
 p "Created #{User.count} user(s)!"
+
+User.all.each_with_index do |user, index|
+  file = File.open(Rails.root.join("app/assets/images/#{index}.jpg"))
+  user.photo.attach(io: file, filename: "#{index}.jpg", content_type: "image/jpg")
+end
+p "Added photos to users."
 
 Climb.create!([
   {
     status: "completed",
     description: "Here is some description.",
     climb_date: Date.today - 100,
-    line: Line.find(105),
+    line: Line.find(43),
     user: User.find_by(username: "Komezu")
-  },
-  {
-    status: "attempted",
-    description: "This one is really hard!",
-    climb_date: Date.today - 5,
-    line: Line.find(7),
-    user: User.find_by(username: "Redvelvet")
-  },
-  {
-    status: "retreated",
-    description: "Backed down at the third bolt because it was too hard :(",
-    climb_date: Date.today - 10,
-    line: Line.find(98),
-    user: User.find_by(username: "Redvelvet")
   },
   {
     status: "completed",
     description: "Here is some description.",
     climb_date: Date.today - 50,
-    line: Line.find(105),
-    user: User.find_by(username: "Nifemi")
+    line: Line.find(43),
+    user: User.find_by(username: "Nifemi21")
+  },
+  {
+    status: "completed",
+    description: "I found this one pretty easy, looks like I'm getting better and better at climbing!",
+    climb_date: Date.today - 10,
+    line: Line.find(1),
+    user: User.find_by(username: "Sandra24")
+  },
+  {
+    status: "attempted",
+    description: "Almost did it, to try again soon!!",
+    climb_date: Date.today - 7,
+    line: Line.find(99),
+    user: User.find_by(username: "Sandra24")
+  },
+  {
+    status: "completed",
+    description: "It's not as bad as they make it sound, a great warmup for sure.",
+    climb_date: Date.today - 5,
+    line: Line.find(69),
+    user: User.find_by(username: "Sandra24")
+  },
+  {
+    status: "retreated",
+    description: "TOO HARD! :(",
+    climb_date: Date.today - 2,
+    line: Line.find(9),
+    user: User.find_by(username: "Sandra24")
   }
 ])
 p "Created #{Climb.count} climb(s)!"
 
 Tip.create!([
   {
-    content: "Middle part was slippery when I climbed it, be careful.",
-    line: Line.find(105),
+    content: "Be very careful at the tricky corner, easy to lose your footing there!",
+    line: Line.find(43),
     user: User.find_by(username: "Komezu")
   },
   {
     title: "Poison Ivy!!",
     content: "Beware of poisonous plants, I saw some poison ivy on my way.",
-    line: Line.find(105),
-    user: User.find_by(username: "Nifemi")
+    line: Line.find(43),
+    user: User.find_by(username: "Nifemi21")
   }
 ])
 p "Created #{Tip.count} tip(s)!"
